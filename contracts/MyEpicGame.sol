606e83e532680f298b6492abadd6a7ba08424281
Andreas Bigger <abigger87@gmail.com>
2021-10-23 09:42:02 -0700

    :sparkles: minting :sparkles:

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -2,10 +2,107 @@
 
 pragma solidity ^0.8.0;
 
+// NFT contract to inherit from.
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions OpenZeppelin provides.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
 import "hardhat/console.sol";
 
-contract MyEpicGame {
-  constructor() {
-    console.log("THIS IS MY GAME CONTRACT. NICE.");
-  }
+// Our contract inherits from ERC721, which is the standard NFT contract!
+contract MyEpicGame is ERC721 {
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+
+    // The tokenId is the NFTs unique identifier, it's just a number that goes
+    // 0, 1, 2, 3, etc.
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+
+    CharacterAttributes[] defaultCharacters;
+
+    // We create a mapping from the nft's tokenId => that NFTs attributes.
+    mapping(uint256 => CharacterAttributes) public nftHolderAttributes;
+
+    // A mapping from an address => the NFTs tokenId. Gives me an ez way
+    // to store the owner of the NFT and reference it later.
+    mapping(address => uint256) public nftHolders;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    )
+        // Below, you can also see I added some special identifier symbols for our NFT.
+        // This is the name and symbol for our token, ex Ethereum and ETH. I just call mine
+        // Heroes and HERO. Remember, an NFT is just a token!
+        ERC721("Heroes", "HERO")
+    {
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+
+            CharacterAttributes memory c = defaultCharacters[i];
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                c.name,
+                c.hp,
+                c.imageURI
+            );
+        }
+
+        // I increment tokenIds here so that my first NFT has an ID of 1.
+        // More on this in the lesson!
+        _tokenIds.increment();
+    }
+
+    // Users would be able to hit this function and get their NFT based on the
+    // characterId they send in!
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        // Get current tokenId (starts at 1 since we incremented in the constructor).
+        uint256 newItemId = _tokenIds.current();
+
+        // The magical function! Assigns the tokenId to the caller's wallet address.
+        _safeMint(msg.sender, newItemId);
+
+        // We map the tokenId => their character attributes. More on this in
+        // the lesson below.
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].hp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log(
+            "Minted NFT w/ tokenId %s and characterIndex %s",
+            newItemId,
+            _characterIndex
+        );
+
+        // Keep an easy way to see who owns what NFT.
+        nftHolders[msg.sender] = newItemId;
+
+        // Increment the tokenId for the next person that uses it.
+        _tokenIds.increment();
+    }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,55 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
-  const gameContract = await gameContractFactory.deploy();
+  const gameContract = await gameContractFactory.deploy(
+    // Names
+    [
+      "Matthias",
+      "Vitch",
+      "Cheesethief",
+      "DarkClaw",
+      "Methuselah",
+      "Friar Hugo",
+      "Abbott Mortimer",
+      "Fangburn",
+      "Deadnose"
+    ],
+    // Images
+    [
+      "https://ibb.co/93N8HQK",
+      "https://ibb.co/8sTf5zv",
+      "https://ibb.co/XjsfjJP",
+      "https://ibb.co/ChfWZK7",
+      "https://ibb.co/cT57yX9",
+      "https://ibb.co/YDyRjHR",
+      "https://ibb.co/JxCjbv3",
+      "https://ibb.co/894drdy",
+      "https://ibb.co/XxQhhtd"
+    ],
+    // HP values
+    [
+      800,
+      300,
+      200,
+      400,
+      600,
+      300,
+      400,
+      700,
+      500
+    ],
+    // Attack damage values
+    [
+      200,
+      75,
+      90,
+      150,
+      125,
+      20,
+      55,
+      170,
+      120,
+    ]
+  );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
